5a5e9a4d2a9c7f4d234364f63efa4a094b0c3073
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2024-05-30 20:04:48 -0300

    modificando-tabla

--- src/screens/students/student-screen.tsx
+++ src/screens/students/student-screen.tsx
@@ -1,18 +1,33 @@
-import { TableProps, Space, Tooltip, Button, Table, Input } from "antd"
-import { LuMessageCircle } from "react-icons/lu"
-import { BiSolidUserDetail } from "react-icons/bi";
+import { TableProps, Space, Tooltip, Button, Table, Input, Modal } from "antd"
+import { MdDelete } from "react-icons/md";
 import { useNavigate } from "react-router-dom"
 import { FaPencil } from "react-icons/fa6"
 import moment from "moment"
 
 import { useDataContext } from "../../context/data"
 import { MdOutlineEmail } from "react-icons/md";
+import { deleteStudent } from "../../api/students";
+
 
 const { Search } = Input
+const { confirm } = Modal;
 
 export const StudentScreen = () => {
   const { students, loading, getStudents } = useDataContext()
   const navigate = useNavigate()
+  const showDeleteConfirm = (studentId: string) => {
+    confirm({
+      title: "¿Estás seguro de que quieres eliminar este alumno?",
+      okText: "Sí",
+      okType: "danger",
+      cancelText: "Cancelar",
+      onOk() {
+        deleteStudent(studentId);
+      }
+    })
+  }
+
+
 
   const columns: TableProps<any>['columns'] = [
     {
@@ -35,7 +50,7 @@ export const StudentScreen = () => {
     { title: 'Escuela', dataIndex: 'school', key: 'school' },
     { title: 'Grupo', dataIndex: 'groups', key: 'groups', render: (_, record) => record.groups?.join('-') },
     { title: 'Profesora', dataIndex: 'teacher', key: 'teacher' },
-    { title: 'Fecha de cumpleaños', dataIndex: 'birth_date', key: 'birth_date', render: (_, record) => moment(record.birth_date).format('DD-MM-YYYY') },
+    { title: 'Cumpleaño', dataIndex: 'birth_date', key: 'birth_date', render: (_, record) => moment(record.birth_date).format('DD-MM-YYYY') },
     { title: 'Tutor a cargo', dataIndex: 'tutor_occupation', key: 'tutor_occupation', width: 200 },
     {
       align: 'center',
@@ -45,17 +60,14 @@ export const StudentScreen = () => {
       width: 200,
       render: (_, record) => (
         <Space>
-          <Tooltip title="Chatear por consulta">
-            <Button type="text" icon={<LuMessageCircle size={18} />} />
-          </Tooltip>
           <Tooltip title="Editar">
             <Button type="text" icon={<FaPencil size={18} />} onClick={() => navigate(`/students/${record._id}`)} />
           </Tooltip>
-          <Tooltip title="Detalles">
-            <Button type="text" icon={<BiSolidUserDetail size={24} />} onClick={() => navigate(`/students/details/${record._id}`)} />
-          </Tooltip>
           <Tooltip title="Enviar un correo">
             <Button type="text" icon={<MdOutlineEmail size={18} />} />
+          </Tooltip>
+          <Tooltip title="Eliminar">
+            <Button type="text" icon={<MdDelete size={24} />} onClick={() => showDeleteConfirm(record._id)} />
           </Tooltip>
         </Space>
       )
