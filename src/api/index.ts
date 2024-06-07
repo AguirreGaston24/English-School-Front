55e6e2a8008e1d65ec4ed7fe85780803a7e07d3f
Alexispavon010 <themaster034@gmail.com>
2024-06-07 18:44:05 -0300

    [fix]: se agarra el token refrescado xd

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000'
 
-const token = localStorage.getItem('token');
 
 const instance = axios.create({
   baseURL: `${BASE_URL}`,
@@ -10,6 +9,7 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
