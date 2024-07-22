d3c76bd18a73efeb190dd7b0206600abb69da8f9
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2024-07-22 18:02:08 -0300

    Modificando-pantalla

--- src/screens/students/student-screen.tsx
+++ src/screens/students/student-screen.tsx
@@ -2,10 +2,10 @@ import { TableProps, Space, Tooltip, Button, Table, Input, Modal, Select } from 
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { MdDelete } from "react-icons/md";
 import { FaPencil } from "react-icons/fa6"
+import { FaWhatsapp } from "react-icons/fa";
 import { toast } from "sonner";
 import moment from "moment"
 
-import { MdOutlineEmail } from "react-icons/md";
 import { ADDRESSES } from "../../constant/address";
 import { GROUPS } from "../../constant/groups";
 import { SCHOOl } from "../../constant/schools";
@@ -43,6 +43,16 @@ export const StudentScreen = () => {
     })
   }
 
+
+
+
+  const handleWhatsAppNavigate = (phoneNumber: string) => {
+    const message = 'Hello, I would like to get more information.';
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
+    window.open(whatsappUrl, '_blank');
+  };
+
   const columns: TableProps<any>['columns'] = [
     {
       title: 'Nombre',
@@ -77,8 +87,8 @@ export const StudentScreen = () => {
           <Tooltip title="Editar">
             <Button type="text" icon={<FaPencil size={18} />} onClick={() => navigate(`/students/${record._id}`)} />
           </Tooltip>
-          <Tooltip title="Enviar un correo">
-            <Button type="text" icon={<MdOutlineEmail size={18} />} />
+          <Tooltip title="Enviar Whatsapp">
+            <Button type="text" icon={<FaWhatsapp size={18} />} onClick={() => handleWhatsAppNavigate(record.phone)} />
           </Tooltip>
           <Tooltip title="Eliminar">
             <Button type="text" icon={<MdDelete size={24} />} onClick={() => showDeleteConfirm(record._id)} />
@@ -90,15 +100,33 @@ export const StudentScreen = () => {
 
   return (
     <div>
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 py-2 mb-5">
-        <Button onClick={() => navigate('/students/new')}>Nuevo alumno</Button>
-        <Search
-          allowClear name="search" placeholder="Buscar..." onSearch={(value) => handleFilterChange([["term", value]])} defaultValue={searchParams.get("term") || ""} />
-        <Select allowClear placeholder='Filtro por barrio' options={ADDRESSES} onChange={(value) => handleFilterChange([['district', value]])} defaultValue={searchParams.get("district") || ""} />
-        <Select allowClear placeholder='Filtro por escuela' options={SCHOOl} onChange={(value) => handleFilterChange([['school', value]])} defaultValue={searchParams.get("school") || ""} />
-        <Select allowClear placeholder='Filtro por grupo' options={GROUPS} onChange={(value) => handleFilterChange([['group', value]])} defaultValue={searchParams.get("group") || ""} />
-        <TeacherSelect allowClear placeholder='Filtro por profe' onChange={(value: string) => handleFilterChange([['teacher', value]])} defaultValue={searchParams.get("teacher") || ""} />
+      <div className="px-4">
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 py-2 mb-5 w-full  items-center ">
+          <Button className="w-full " onClick={() => navigate('/students/new')}>Agregar alumno</Button>
+          <div className="w-full">
+            <p className="mb-3">Buscar de manera general</p>
+            <Search
+              allowClear name="search" placeholder="Buscar..." onSearch={(value) => handleFilterChange([["term", value]])} defaultValue={searchParams.get("term") || ""} className="w-full" />
+          </div>
+          <div className="w-full">
+            <p className="mb-3">Filtra por barrio</p>
+            <Select allowClear placeholder='Busca por barrio' options={ADDRESSES} onChange={(value) => handleFilterChange([['district', value]])} defaultValue={searchParams.get("district") || ""} className="w-full" />
+          </div>
+          <div className="w-full">
+            <p className="mb-3">Filtra por escuela</p>
+            <Select allowClear placeholder='Busca por escuela' options={SCHOOl} onChange={(value) => handleFilterChange([['school', value]])} defaultValue={searchParams.get("school") || ""} className="w-full" />
+          </div>
+          <div className="w-full">
+            <p className="mb-3">Filtra por grupo</p>
+            <Select allowClear placeholder='Busca por grupo' options={GROUPS} onChange={(value) => handleFilterChange([['group', value]])} defaultValue={searchParams.get("group") || ""} className="w-full" />
+          </div>
+          <div className="w-full">
+            <p className="mb-3">Filtra por profe</p>
+            <TeacherSelect allowClear placeholder='Busca por profe' onChange={(value: string) => handleFilterChange([['teacher', value]])} defaultValue={searchParams.get("teacher") || ""} className="w-full" />
+          </div>
+        </div>
       </div>
+      <h1 className="text-2xl font-bold mb-5">Alumnos Registrados:</h1>
       <Table
         size="small"
         dataSource={students}
