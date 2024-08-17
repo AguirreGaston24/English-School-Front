20aaf96f654eae67bb62ed367679775db8e2fd57
Alexispavon010 <themaster034@gmail.com>
2024-08-16 21:25:22 -0300

    change value becas

--- src/constant/becas.ts
+++ src/constant/becas.ts
@@ -1,52 +1,52 @@
 export const BECAS = [
   {
     label: "%10",
-    value: "10%",
+    value: 0.10,
     id: 1
   },
   {
     label: "%15",
-    value: "%15",
+    value: 0.15,
     id: 2
   },
   {
     label: "%20",
-    value: "%20",
+    value: 0.15,
     id: 3
   },
   {
     label: "%25",
-    value: "%25",
+    value: 0.25,
     id: 4
   },
   {
     label: "%30",
-    value: "%30",
+    value:  0.30,
     id: 5
   },
   {
     label: "%35",
-    value: "%35",
+    value:  0.35,
     id: 6
   },
   {
     label: "%40",
-    value: "%40",
+    value:  0.40,
     id: 7
   },
   {
     label: "%45",
-    value: "%45",
+    value:  0.45,
     id: 8
   },
   {
     label: "%50",
-    value: "%50",
+    value:  0.50,
     id: 8
   },
   {
     label: "%55",
-    value: "%55",
+    value:  0.55,
     id: 8
   }
 ]
