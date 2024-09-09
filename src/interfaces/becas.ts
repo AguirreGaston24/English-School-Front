5f470c092c4850791adc65c2b44d482e19ddbc4d
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2024-09-09 19:06:18 -0300

    agregando-nuevas-interfaces

---
+++ src/interfaces/becas.ts
@@ -0,0 +1,5 @@
+export interface Becas {
+  name: string,
+  price: number
+}
+
---
+++ src/interfaces/groups.ts
@@ -0,0 +1,8 @@
+export interface IGroups {
+  id: number,
+  level: string,
+  teacher: string,
+  startDate: string,
+  endDate: string,
+  days: string[],
+}
