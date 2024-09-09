a2029c8b19af6ca870c19ca214df9aeba37af52a
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2024-09-09 19:07:37 -0300

    modificando-titulos-de-los-niveles

--- src/constant/levels.ts
+++ src/constant/levels.ts
@@ -1,47 +1,72 @@
 export const LEVELS = [
   {
-    label: "PRE-STARTER",
-    value: "PRE-STARTER",
+    label: "KINDERGARTEN",
+    value: "KINDERGARTEN",
     id: 1
+  },
+  {
+    label: "1ST GRADE",
+    value: "1ST GRADE",
+    id: 2
   },
   {
     label: "STARTER",
     value: "STARTER",
-    id: 2
-  },
-  {
-    label: "ELEMENTARY",
-    value: "ELEMENTARY",
     id: 3
   },
   {
-    label: "PRE-INTERMEDIATE",
-    value: "PRE-INTERMEDIATE",
+    label: "JUNIOR",
+    value: "JUNIOR",
     id: 4
   },
   {
-    label: "INTERMEDIATE",
-    value: "INTERMEDIATE",
+    label: "PRE-INTERMEDIATE KIDS",
+    value: "PRE-INTERMEDIATE KIDS",
     id: 5
   },
   {
-    label: "UPPER-INTERMEDIATE",
-    value: "UPPER-INTERMEDIATE",
+    label: "INTERMEDIATE SPOKEN ENGLISH",
+    value: "INTERMEDIATE SPOKEN ENGLISH",
     id: 6
+  },
+  {
+    label: "UPPER INTERMEDIATE",
+    value: "UPPER INTERMEDIATE",
+    id: 7
   },
   {
     label: "ADVANCED",
     value: "ADVANCED",
-    id: 7
+    id: 8
+  },
+  {
+    label: "PRE-STARTER",
+    value: "PRE-STARTER",
+    id: 10
   },
   {
     label: "PREPARATORY",
     value: "PREPARATORY",
-    id: 8
+    id: 11
   },
   {
-    label: "KINDERGARTEN",
-    value: "KINDERGARTEN",
-    id: 8
-  }
+    label: "PRIMARY",
+    value: "PRIMARY",
+    id: 12
+  },
+  {
+    label: "ELEMENTARY",
+    value: "ELEMENTARY",
+    id: 13
+  },
+  {
+    label: "INTERMIDATE",
+    value: "INTERMIDATE",
+    id: 14
+  },
+  {
+    label: "UPPER INTERMEDIATE",
+    value: "UPPER INTERMEDIATE",
+    id: 15
+  },
 ]
