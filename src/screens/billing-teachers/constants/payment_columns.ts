16303fa6ec3725981b6d579508278d8f338424dc
Alexispavon010 <themaster034@gmail.com>
2024-09-25 18:04:09 -0300

    update payment

--- src/screens/billing-teachers/constants/payment_columns.ts
+++ src/screens/billing-teachers/constants/payment_columns.ts
@@ -1,6 +1,5 @@
 export const PAYMENT_COLUMNS = [
   { title: 'Grupo', dataIndex: 'group', key: 'group' },
-  { title: 'Profesor/a', dataIndex: 'teacher', key: 'teacher' },
   { title: 'Alumnos', dataIndex: 'total_students', key: 'total_students' },
-  { title: 'Monto Total', dataIndex: 'teacherEarnings', key: 'teacherEarnings', render: (_: any, record: any) => `$${record.teacherEarnings.toFixed(2)}` },
+  { title: 'Monto Total', dataIndex: 'total_amount', key: 'total_amount', render: (_: any, record: any) => `$${record.total_amount?.toFixed(2)}` },
 ]
--- src/screens/billing-teachers/index.tsx
+++ src/screens/billing-teachers/index.tsx
@@ -1,15 +1,17 @@
-import { Card, Form, Input, Table } from 'antd'
+import { Card, Descriptions, Form, Input, Table } from 'antd'
 import { useState } from 'react'
 
+import { BILLING_COLUMNS } from '../billing-students/constants/billing_columns'
 import { PAYMENT_COLUMNS } from './constants/payment_columns'
 import TeacherSelect from '../../components/teacher-select'
-import { useGroupContext } from '../../context/group'
 import { getPaymentById } from '../../api/payments'
+import { ITeacher } from '../../interfaces/teacher'
 
 export const BillingTeachersScreen = () => {
-  const { groups, handleFilterChange } = useGroupContext()
   const [loading, setLoading] = useState(false)
-  const [data, setData] = useState([])
+  const [teacher, setTeacher] = useState<ITeacher | undefined>()
+  const [groups, setGroups] = useState([])
+  const [billings, setBillings] = useState([])
 
   const handleSelectTeacher = (value: string) => {
     setLoading(true)
@@ -17,7 +19,9 @@ export const BillingTeachersScreen = () => {
     getPaymentById(value)
       .then(({ data }) => {
         console.log(data, 'data')
-        setData(data.results)
+        setTeacher(data.teacher)
+        setGroups(data.groups)
+        setBillings(data.billings)
       })
       .catch((error) => {
         console.log(error)
@@ -47,11 +51,35 @@ export const BillingTeachersScreen = () => {
             </div>
           </Form>
         </Card>
+        <Card title="Detalles del Profesor" className="col-span-1 lg:col-span-4 p-5">
+          <Descriptions bordered column={1}>
+            <Descriptions.Item label="Nombre">
+              {teacher?.firstname} {teacher?.lastname}
+            </Descriptions.Item>
+            <Descriptions.Item label="Email">{teacher?.email}</Descriptions.Item>
+            <Descriptions.Item label="Teléfono">{teacher?.phone}</Descriptions.Item>
+            <Descriptions.Item label="Ciudad">{teacher?.city}</Descriptions.Item>
+            <Descriptions.Item label="Dirección">{teacher?.address}</Descriptions.Item>
+            <Descriptions.Item label="Distrito">{teacher?.district}</Descriptions.Item>
+            <Descriptions.Item label="DNI">{teacher?.dni}</Descriptions.Item>
+            <Descriptions.Item label="Fecha de Nacimiento">
+              {teacher?.birth_date ? new Date(teacher.birth_date).toLocaleDateString() : '--/--/--'}
+            </Descriptions.Item>
+          </Descriptions>
+        </Card>
+        <Card title='Detalle del recibo' className="col-span-1 lg:col-span-4">
+          <Table
+            className="col-span-1 md:col-span-3"
+            dataSource={billings}
+            scroll={{ x: 800 }}
+            columns={BILLING_COLUMNS}
+          />
+        </Card>
         <Card title='Detalle del recibo' className="col-span-1 lg:col-span-4">
           <Table
             className="col-span-1 md:col-span-1"
             columns={PAYMENT_COLUMNS}
-            dataSource={data}
+            dataSource={groups}
             scroll={{ x: 200 }}
             loading={loading}
             pagination={{
