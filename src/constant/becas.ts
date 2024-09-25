008b00aa1d76567e042796f4ed6b3167f4b19529
Alexispavon010 <themaster034@gmail.com>
2024-09-25 15:15:17 -0300

    add constant becas

--- src/constant/becas.ts
+++ src/constant/becas.ts
@@ -1,28 +1,37 @@
 export const BECAS = [
   {
-    label: "INDIVIDUAL / TRANSFERENCIA",
-    name: "INDIVIDUAL / TRANSFERENCIA",
-    value: 2500,
+    label: "INDIVIDUAL",
+    name: "INDIVIDUAL",
+    value: "INDIVIDUAL",
+    price: 2500,
     id: 1
   },
   {
     label: "HERMANOS",
     name: "HERMANOS",
-    value: 2000,
+    value: "HERMANOS",
+    price: 2000,
     id: 2
   },
   {
     label: "PRIMOS",
     name: "PRIMOS",
-    value: 2200,
+    value: "PRIMOS",
+    price: 2200,
     id: 3
   },
   {
     label: "BECA ANUAL",
     name: "BECA ANUAL",
-    value: 1500,
+    value: "BECA_ANUAL",
+    price: 1500,
     id: 4
   },
-
-  
+  {
+    label: "TRANSFERENCIA",
+    name: "TRANSFERENCIA",
+    value: "TRANSFERENCIA",
+    price: 2500,
+    id: 5
+  },
 ]
