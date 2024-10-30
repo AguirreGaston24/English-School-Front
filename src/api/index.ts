10633bcbd1c2d7e6dfc7c4f5bf9c6cf26f557176
AguirreGaston24 <aguirregastona@hotmail.com>
2024-10-29 22:03:07 -0300

    deploy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:4000'
+export const BASE_URL ='https://english-school-back24.vercel.app/'
 
 
 const instance = axios.create({
